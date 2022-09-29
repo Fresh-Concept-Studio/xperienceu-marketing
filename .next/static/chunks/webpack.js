c6c46b4bcb40262d427d77639b2e4e86c5a401b3
freshconceptstudio <73487121+freshconceptstudio@users.noreply.github.com>
2022-09-29 15:49:24 -0600

    Adjust config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3da445c031d3edef"; }
+/******/ 		__webpack_require__.h = function() { return "013783a1d40074d2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
